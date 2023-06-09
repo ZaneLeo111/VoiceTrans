405755232457d61b50cb5c43222306bb85044f97
ZeLiu1 <90260431+ZeLiu1@users.noreply.github.com>
2023-06-09 13:46:05 -0230

    Add logo and change color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useState } from "react";
+import { close, menu, logo } from "../assets";
+import { navLinks } from "../constants";
 
 const Navbar: React.FC = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} />
+    </nav>
+  );
 };
 
 export default Navbar;
