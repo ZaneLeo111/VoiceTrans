8c4d3b2bda454e0b5d154248a2bb92ccbab1e40f
ZeLiu1 <90260431+ZeLiu1@users.noreply.github.com>
2023-07-12 23:49:48 -0230

    Connect frontend to backend

--- VoiceTrans/VoiceTrans/settings.py
+++ VoiceTrans/VoiceTrans/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -117,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/assets/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'dist', 'assets')]
 
 # Default primary key field type
